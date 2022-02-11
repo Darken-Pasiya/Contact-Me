b5975e53e44e86ff36b3dd56996cfcc085559982
Darken-Pasiya <91953654+Darken-Pasiya@users.noreply.github.com>
2022-02-11 20:51:47 +0530

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+z="
+";Jz=' \e[';Tz='"   ';bz='act';az='Cont';Pz='ss ᴇ';Hz='    ';Yz='slee';Gz='m   ';Lz='mCon';Xz='ᴛᴏᴏʟ';Zz='p 1';Sz='"';Dz=' -e ';Cz='echo';Bz='r';Az='clea';Qz='ɴᴛᴇʀ';Kz='1;35';Vz='ᴜɴ ᴛ';Fz='1;33';Mz='tact';Rz=' ᴛᴏ ';Iz='ᴛʏᴘᴇ';Wz='ʜɪs ';Uz='   ʀ';Oz=' ᴘʀᴇ';Ez='"\e[';Nz='mᴀɴᴅ';
+eval "$Az$Bz$z$Cz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Jz$Fz$Nz$Oz$Pz$Qz$Rz$Sz$z$Cz$Dz$Tz$Hz$Hz$Hz$Uz$Vz$Wz$Xz$Sz$z$Yz$Zz$z$az$bz"
